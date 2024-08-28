1e4cee6eedce3cb577b620495f5e29944635758f
Teddy Medjio <nganmedjiojeanclaude@gmail.com>
2024-08-28 12:51:19 +0100

    shulder

--- app/(dashboard)/teacher/page.tsx
+++ app/(dashboard)/teacher/page.tsx
@@ -7,7 +7,7 @@ export default function TeacherPage() {
       {/*LEFT*/}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule</h1>
+          <h1 className="text-xl font-semibold">Schedule(0)</h1>
           <BigCalender />
         </div>
       </div>
